8ebcd21966f4bdc12a92dc3eeaf6b29f0cdd0b1a
alexeyborisov123 <44069275+alexeyborisov123@users.noreply.github.com>
2018-11-25 21:33:28 +0300

    ...

---
+++ 25.11.2018/1_zadanie_4_domashnyaya_rabota.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+cout « "2018 * x + 2019 * y = 78487894" « endl;
+
+cout « "x = 37073 y = 1820" « endl;
+
+cout « "x = 35054 y = 3838" « endl;
+
+cout « "x = 33035 y = 5856" « endl;
+
+cout « "x = 31016 y = 7874" « endl;
+
+cout « "x = 28997 y = 9892" « endl;
+
+cout « "x = 26978 y = 11910" « endl;
+
+cout « "x = 24959 y = 13928" « endl;
+
+cout « "x = 22940 y = 15946" « endl;
+
+cout « "x = 20921 y = 17964" « endl;
+
+cout « "x = 18902 y = 19982" « endl;
+
+cout « "x = 16883 y = 22000" « endl;
+
+cout « "x = 14864 y = 24018" « endl;
+
+cout « "x = 12845 y = 26036" « endl;
+
+cout « "x = 10826 y = 28054" « endl;
+
+cout « "x = 8807 y = 30072" « endl;
+
+cout « "x = 6788 y = 32090" « endl;
+
+cout « "x = 4769 y = 34108" « endl;
+
+cout « "x = 2750 y = 36126" « endl;
+
+cout « "x = 731 y = 38144";
+
+return 0;
+
+}
